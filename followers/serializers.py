4e7a269d5fab346beffb15999b93ad21e2a01e1b
Yuss76A <yuse_91yus@hotmail.com>
2025-03-14 13:17:06 +0100

    Added FollowerList and FollowerDetail

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from models import Follower
+from .models import Follower
 
 
 class FollowerSerializer(serializers.ModelSerializer):

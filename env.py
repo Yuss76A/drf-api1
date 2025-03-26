25c58a602eddedd274dfc7967c21da0f9a51af06
Yuss76A <yuse_91yus@hotmail.com>
2025-03-26 21:32:28 +0100

    Setting project to be deployed

--- env.py
+++ env.py
@@ -1,4 +1,6 @@
 import os
 
-os.environ['CLOUDINARY_URL'] ='cloudinary://<your_api_key>:<your_api_secret>@dy5g22zlq'
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:nvNd4rDW7MUa@ep-crimson-violet-a2o6l5pt.eu-central-1.aws.neon.tech/stock_yearn_fiber_505768"
+os.environ['CLOUDINARY_URL'] = 'cloudinary://<your_api_key>:<your_api_secret>@dy5g22zlq'
 os.environ['DEV'] = '1'
+os.environ.setdefault("SECRET_KEY", "-2oyk_5#*dym+=t1uaalacvk3&qylsh0&rhd6$a5-)++t==rq_")

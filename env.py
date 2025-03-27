25c8d4928d87dfc3747ba05943b66e82b774d693
Yuss76A <yuse_91yus@hotmail.com>
2025-03-27 01:25:22 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ['CLOUDINARY_URL'] = 'cloudinary://<your_api_key>:<your_api_secret>@dy5g22zlq'
+os.environ['CLOUDINARY_URL'] =
 os.environ['DEV'] = '1'
-os.environ.setdefault("SECRET_KEY", "-2oyk_5#*dym+=t1uaalacvk3&qylsh0&rhd6$a5-)++t==rq_")
+os.environ.setdefault("SECRET_KEY")
